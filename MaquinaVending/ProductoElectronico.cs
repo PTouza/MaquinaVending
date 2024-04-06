83620e53104721d47697f00a9366b8e8fae47bf1
PTouza <tjpablo05@gmail.com>
2024-04-06 14:05:48 +0200

    comentarios

--- MaquinaVending/ProductoElectronico.cs
+++ MaquinaVending/ProductoElectronico.cs
@@ -8,12 +8,15 @@ namespace MaquinaVending
 {
     internal class ProductoElectronico : Producto
     {
+        // PROPIEDADES
         public bool Tiene_Bateria { get; set; }
         public bool Precargado { get; set; }
 
+        // CONSTRUCTORES
         public ProductoElectronico() { }
 
-        public ProductoElectronico (string nombre, int unidades, double precio_Unitario, string descripcion, bool tiene_Bateria, bool precargado) : base (nombre, unidades, precio_Unitario, descripcion)
+        public ProductoElectronico (string nombre, int unidades, double precio_Unitario, string descripcion, 
+            bool tiene_Bateria, bool precargado) : base (nombre, unidades, precio_Unitario, descripcion)
         {
             Tiene_Bateria = tiene_Bateria;
             Precargado = precargado;
