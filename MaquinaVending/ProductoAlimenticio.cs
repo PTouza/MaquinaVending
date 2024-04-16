907a9d7a857ca15f8aefb72a8bc8086ee4ef96b8
Javier de Lorenzo Garrobo <9306503@alumnos.ufv.es>
2024-04-16 08:59:52 +0200

    cambios javi

--- MaquinaVending/ProductoAlimenticio.cs
+++ MaquinaVending/ProductoAlimenticio.cs
@@ -8,12 +8,13 @@ namespace MaquinaVending
 {
     internal class ProductoAlimenticio : Producto
     {
-        public string Informacion_Nutricional {  get; set; }
+        public string Calorias { get; set; }
+        public string Grasa { get; set; }
+        public string Azucar { get; set; }
 
-        // CONSTRUCTORES
         public ProductoAlimenticio() { }
 
-        public ProductoAlimenticio(string nombre, int unidades, double precio_Unitario, string descripcion, string info_nutricional) : base (nombre, unidades, precio_Unitario, descripcion)
+        public ProductoAlimenticio(string nombre, int unidades, double precio_Unitario, string descripcion, string calorias, string grasa, string azucar) : base(nombre, unidades, precio_Unitario, descripcion)
         {
             Calorias = calorias;
             Grasa = grasa;
